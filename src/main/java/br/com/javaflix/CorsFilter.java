ef17beb86a224651268e877bc0c0c69936f0f11b
walkowicz19 <mnwmatheus@gmail.com>
2026-03-23 21:34:37 -0300

    fix: adiciona custom CORS filter em Java nativo para bypass da porta 5173

---
+++ src/main/java/br/com/javaflix/CorsFilter.java
@@ -0,0 +1,26 @@
+package br.com.javaflix;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        String origin = requestContext.getHeaderString("Origin");
+        if (origin != null && !origin.isEmpty()) {
+            responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", origin);
+        } else {
+            responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
+        }
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
